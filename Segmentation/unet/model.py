db4a7f1805d1e3079ed4b0c30e7bc261d70d1023
yas-hub-tub <yastrauss29@gmail.com>
2026-09-08 11:29:31 +0300

    Update model.py

--- Segmentation/unet/model.py
+++ Segmentation/unet/model.py
@@ -14,21 +14,6 @@ from keras.applications.vgg16 import VGG16
 # print("tf.__version__ is", tf.__version__)
 # print("tf.keras.__version__ is:", tf.keras.__version__)
 
-def _get_available_gpus():
-    """Get a list of available gpu devices (formatted as strings).
-
-    # Returns
-        A list of available GPU devices.
-    """
-    #global _LOCAL_DEVICES
-    if tfback._LOCAL_DEVICES is None:
-        devices = tf.config.list_logical_devices()
-        tfback._LOCAL_DEVICES = [x.name for x in devices]
-    return [x for x in tfback._LOCAL_DEVICES if 'device:gpu' in x.lower()]
-
-tfback._get_available_gpus = _get_available_gpus
-
-
 def binary_focal_loss(gamma=2, alpha=0.99):
     """
     Binary form of focal loss.
